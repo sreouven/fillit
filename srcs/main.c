df2ccdb2d4a0b210334cefc3c889e8c629ce896e
sreouven <reouven.sellam@gmail.com>
2017-11-19 22:47:16 +0100

    .

--- srcs/main.c
+++ srcs/main.c
@@ -24,23 +24,18 @@ int		open_file(char **argv, char *buf)
 }
 
 /*
-   CHECK_CHAR verifie l'integrite du buffer au niveau des char . # \n et %4#
+   CHECK_CHAR verifie l'integrite du buffer au niveau des char . # \n
 0: erreur, 1: reussite
  */
 int		check_char(char *buf, int size_buf)
 {
-		int		nbh;
-
-		nbh = 0;
 		while(*buf)
 		{
 				if (*buf != '.' && *buf != '#' && *buf != '\n')
 						return (0);
-				if (*buf == '#')
-						nbh++;
-				buf++;															//pas forcement neilleur endroit
+				buf++;
 		}
-		return ((nbh % 4 == 0) ? 1 : 0);
+		return (1);
 }
 
 /*
@@ -79,11 +74,11 @@ int		check_format(char *buf, int	size_buf)
    FT_OPEN_CHECK_FILE ouvre et verifie integrite du format du fichier
 0: error, 1: reussite
  */
-int		ft_open_check_file(char **argv)
+char		*ft_open_check_file(char **argv)
 {
 		char	buf[BUF_SIZE + 1];
+		char	*buf_work;
 		int		len_buffer;
-
 
 		if ((len_buffer = open_file(argv, buf)) == 0)
 				return (0);
@@ -91,75 +86,140 @@ int		ft_open_check_file(char **argv)
 				return (0);
 		if (!(check_format(buf, len_buffer)))
 				return(0);
-		ft_putstr(buf); 																			// debug
-		return (1);
+		if(!(buf_work = malloc(sizeof(char) * len_buffer)))
+				return (NULL);
+		ft_strcpy(buf_work, buf);
+		return (buf_work);
 }
 
 /*************************************************************************************************************/
-int		check_piece(char **tetri)  // ne gere pas 5 cas
+int		check_piece(int	ligne, int colonne, char tetri[][colonne])
 {
 		int		i;
 		int		j;
-		int		nbh;
 		int		ok;
+		int		nbh;
 
-		nbh = 0;
 		ok = 0;
 		j = 0;
-		while (j < 3)
+		nbh = 0;
+		while (j <= ligne - 1)
 		{
 				i = 0;
-				while (i < 3)
+				while (i <= colonne - 2)
 				{
 						if (tetri[j][i] == '#')
 						{
+								ok += check_link(j, i, tetri);
 								nbh++;
-								if ((tetri[j][i + 1] == '#') || (tetri[j + 1][i] == '#'))
-										ok++;
 						}
 						i++;
 				}
 				j++;
 		}
-		return ((nbh == 4 && ok == 3) ? 1 : 0);
+		return ((ok < 6 || nbh != 4) ? 0 : 1);
 }
 
-t_tetri		add_piece(t_tetri *piece, char *block, int ipiece)
+int		check_link(int j, int i, char tetri[][5])
 {
-		t_tetri		*newpiece;
+		int		link;
 
-		if(!(newpiece = malloc(sizeof(t_tetri))))
+		link = 0;
+		if(tetri[j][i - 1] == '#' && (i - 1 >= 0))
+				link++;
+		if(tetri[j][i + 1] == '#' && (i + 1 < 4))
+				link++;
+		if(tetri[j + 1][i] == '#' && (j + 1 < 4))
+				link++;
+		if(tetri[j - 1][i] == '#' && (j - 1 >= 0))
+				link++;
+		return(link);
+}
+
+t_tetri		*new_piece(char *buffer, int ipiece)
+{
+		t_tetri	*new;
+		int		i;
+		int		j;
+		int		k;
+
+		j = 0;
+		k = 21 * ipiece;
+		if(!(new = malloc(sizeof(t_tetri))))
 				return (NULL);
-		newpiece->block = block;
-		newpiece->index = ipiece;
+		while(j < 4)
+		{
+				i = 0;
+				while(i < 5)
+				{
+						new->block[j][i] = buffer[k];
+						k++;
+						i++;
+				}
+				j++;
+		}
+		new->index = ipiece;
+		new->next = NULL;
+		return (new);
+}
 
+void	ft_listadd(t_tetri **alst, t_tetri *new)
+{
+	if (alst && new)
+	{
+		new->next = *alst;
+		*alst = new;
+	}
 }
 
 t_tetri		*fill_list(char *buffer, int sizebuf)
 {
-		t_tetri		*piece;
+		t_tetri		*list;
 		int			ipiece;
 		int			max;
 
 		ipiece = 0;
-		piece = NULL;
 		max = (sizebuf / 21 + 1);
+		if(!(list = malloc(sizeof(t_tetri*) * max)))
+						return (NULL);
+		list = NULL;
 		while (ipiece < max)
 		{
-			add_piece(piece, ft_strsub(buffer -= 20, 20), (max - 1));
-			check_piece();
-			buffer--;
-			ipiece++;
+				ft_listadd(&list, new_piece(buffer, ipiece));
+				if (!(check_piece(4, 5, list->block)))
+				{
+						return (NULL);
+				}
+				ipiece++;
 		}
-		return (ptr_list);
+		return (list);
 }
 /*************************************************************************************************************/
 /* backtracking + print_resulst */
+
+char	**init_tab(int sqrt)
+{
+		char	tab[][];
+
+		while(j < sqrt)
+		{
+				while(i < sqrt)
+				{
+						tab[j][i] = '.';
+						i++;
+				}
+				tab[j][i] = '\0';
+				j++;
+		}
+		return(tab);
+}
+
+void	print_tab(char ***tab)
+{
+
+}
 /*************************************************************************************************************/
 
-/*
-   MESSAGE ERREUR
- */
 void	ft_error(void)
 {
 		write(2, "error\n", 6);
@@ -170,17 +230,23 @@ void	ft_error(void)
 int		main(int argc, char **argv)
 {
 		t_tetri		*ptr_list;
+		char		*buffer;
 
 		if (argc != 2)
 		{
 				write(2, "usage : ./fillit file_name\n", 27);
 				return (1);
 		}
-		if (!(ft_open_check_file(&argv[1])))
+		if(!(buffer = ft_open_check_file(&argv[1])))
 		{
 				ft_error();
 				return (1);
 		}
-		ptr_list = fill_list(
+		ptr_list = fill_list(buffer, ft_strlen(buffer));
+		if(ptr_list == NULL)
+		{
+				ft_error();
+				return (1);
+		}
 		return (0);
 }
